47a2d01b7b4d483ff883343dbe4282bcb8cbdf01
Alauddiin <75433873+Alauddiin@users.noreply.github.com>
2021-07-14 00:50:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ const getUserAmount = function(){
     }else{
         amount = inputVal.value;
         percentage = inputPercentage.value
-        let discountAmount = getDiscount(parseFloat((percentage/100)));
+        let discountAmount = getDiscount(Math.abs((percentage/100)));
         let customerDiscount = Math.abs(discountAmount(amount));
         console.log(customerDiscount);
-        let saveAmount =parseFloat(amount-customerDiscount);
+        let saveAmount =Math.abs(amount-customerDiscount);
         let saveAmntMsg= document.createTextNode(`Congratulations! You have saved: ${saveAmount}TK & discounted amount is ${customerDiscount}TK.`)
         savedAmountPara.className="saved-amount-para"
         savedAmountPara.style.display="block"
